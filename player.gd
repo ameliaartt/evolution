cdd3fe92a325787ed255868852f3955a2e656888
amelaart <ulianaborovsckaya@yandex.ru>
2023-11-12 18:35:41 +0300

    new script

--- player.gd
+++ player.gd
@@ -8,12 +8,20 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
+	position.x = clamp(position.x, 0, 1000)
+	position.y = clamp(position.y, 0, 1000)
+	
 func pick(food):
 	food += 1
 
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
