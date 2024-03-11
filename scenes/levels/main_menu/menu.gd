412e054d757713b447b46d7876b9bea1337f7ad1
Inex Code <gitea@inex.rocks>
2024-03-12 00:32:39 +0300

    fix exit game button

--- scenes/levels/main_menu/menu.gd
+++ scenes/levels/main_menu/menu.gd
@@ -2,6 +2,9 @@ extends Control
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level1/cat_scene_1.tscn")
-	pass # Replace with function body.
 
 
+
+
+func _on_exit_game_button_pressed():
+	get_tree().quit()
